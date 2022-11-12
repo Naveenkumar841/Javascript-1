e8a1262858182de66df3554f2df9a890c0979182
Naveen Kumar <nk499718@gmail.com>
2022-11-12 16:12:46 +0500

    copy

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+//alert(num1 +'\n' + num2 +'\n' + num3+'\n' + num4);
+
+
+
+
+//Calculator//
+
+////Addition
+/*
+var num1 = prompt("Enter your 1st Number");
+var num2 = prompt("Enter your 2nd Number");
+
+var add = num1 + num2;
+
+var sub = num1 - num2;
+
+var multi = num1 * num2;
+
+var div = num1 / num2;
+
+alert(sub +'\n' + add +'\n' + multi+'\n' + div);
+*/
+
+
+
+
+/*
+//Substraction
+
+var sub1 = prompt("Enter your 1st Substraction Number");
+var sub2 = prompt("Enter your 2ns Substraction Number");
+
+var sub3 = sub1 - sub2;
+
+//multiplication
+
+var multi1 = prompt("Enter your 1st Multiplication Number");
+var multi2 = prompt("Enter your 2nd Multiplication Number");
+
+var multi3 = multi1 * multi2;
+
+
+//Division
+var div1 = prompt("Enter your 1st Division Number");
+var div2 = prompt("Enter your 2nd Division Number");
+
+var div3 = div1 / div2;
+
+alert("Addition is " + num3 + "\n" + "Substraction is " + sub3 + "\n" + "Multiplication is " + multi3 + "\n" + "Division is " + div3);
+*/
